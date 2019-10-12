7a730bbaa97fae7af32b05499c65f0e424089ab6
dreamwebspace <martin@dreamweb.space>
2019-10-12 10:10:45 +0100

    auto-update 2019-10-12-10:10:45

--- js.js
+++ js.js
@@ -1,35 +1,42 @@
-var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+var numbers = [1,2,3,4,5];
 var won = [];
 var a;
 var b;
+var c;
 var i=0;
-for (i;i<5;i++) {
-  
-  a = (Math.ceil(Math.random() *6));
-//  a=8;
-    console.log("wylosowana liczba");
-    console.log(a);
-  
+
+console.log(numbers);
+for (i;i<4;i++) 
+
+
+{
+  c = numbers.length;
+  a = (Math.floor(Math.random() *c));
+    console.log("a: "+a);
   b = numbers[a-1];
 //  console.log("pozycja");
- // console.log(b);
-  if (b!=null) {
+//  console.log(b);
+//  
     
   won.push(b);
-  delete numbers[b];
-  }
+  numbers.splice(b,1);
+  console.log(numbers);
+  
+  console.log(won);
+
+  
   
 }
 //  console.log("dlugosc puli");
 //  console.log(numbers.length);
 //  console.log("dlugosc wygranych");
 //  console.log(won.length);
-
-  console.log("numbers");
-  console.log(numbers);
-  console.log("won");
-  console.log(won);
-  
+//
+//  console.log("numbers");
+//  console.log(numbers);
+//  console.log("won");
+//  console.log(won);
+//  
   
   
   
